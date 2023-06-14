12fa189a340a0d99e1cc28b4473542caa0bc67eb
Addison Alvey-Blanco <aja13@illinois.edu>
2023-06-13 21:25:40 -0500

    Fix issues pointed out by mypy, flake8

--- loopy/schedule/__init__.py
+++ loopy/schedule/__init__.py
@@ -704,10 +704,12 @@ def get_insns_in_topologically_sorted_order(
     from pytools.graph import compute_topological_order
 
     rev_dep_map: Dict[str, Set[str]] = {
-            insn.id: set() for insn in kernel.instructions}
+            insn.id: set() for insn in kernel.instructions
+            if isinstance(insn.id, str)}
     for insn in kernel.instructions:
         for dep in insn.depends_on:
-            rev_dep_map[dep].add(insn.id)
+            if insn.id is not None:
+                rev_dep_map[dep].add(insn.id)
 
     # For breaking ties, we compare the features of an intruction
     # so that instructions with the same set of features are lumped
@@ -2089,7 +2091,9 @@ def _generate_loop_schedules_inner(
 
             schedule=(),
 
-            unscheduled_insn_ids={insn.id for insn in kernel.instructions},
+            unscheduled_insn_ids={insn.id
+                                  for insn in kernel.instructions
+                                  if isinstance(insn.id, str)},
             scheduled_insn_ids=frozenset(),
             within_subkernel=kernel.state != KernelState.LINEARIZED,
             may_schedule_global_barriers=True,
